7cfd5414360be4e8796ebdd274e8a6d801f8e5b5
theotherflaneur <66705993+the-other-flaneur@users.noreply.github.com>
2022-06-19 17:55:19 -0400

    Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
 
+import Link from 'next/link';
+import Date from '../components/Date';
+
 import { getSortedPostData } from '../lib/posts';
 
 export async function getStaticProps() {
@@ -31,12 +34,14 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
